d3d79a15d8a361a791b59aec520e3ed39333a1a9
Oguzhan Tasimaz <oguzhantasimaz@Oguzhans-MBP.home>
2022-03-24 03:43:40 +0300

    Refactor: Logger for better output

--- pkg/middlewares/logging/logging.go
+++ pkg/middlewares/logging/logging.go
@@ -1,15 +1,30 @@
 package logging
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"net/http/httptest"
+	"net/http/httputil"
 )
 
 func Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Do stuff here
-		log.Println(r.URL.Query())
-		// Call the next handler, which can be another middleware in the chain, or the final handler.
+		x, err := httputil.DumpRequest(r, true)
+		if err != nil {
+			http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+			return
+		}
+		log.Println(fmt.Sprintf("%q \n", x))
+		rec := httptest.NewRecorder()
+		log.Println(fmt.Sprintf("%q \n", rec.Body))
+
+		// this copies the recorded response to the response writer
+		for k, v := range rec.HeaderMap {
+			w.Header()[k] = v
+		}
+		w.WriteHeader(rec.Code)
+		rec.Body.WriteTo(w)
 		next.ServeHTTP(w, r)
 	})
 }
